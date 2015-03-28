f99f64f02568144789e5beff987e7bf76447f580
Rohan Varma <rnvarma@andrew.cmu.edu>
2015-03-28 17:53:03 -0400

    added bitchin templates and static files to increase badassness of our app

--- puzzlehunt/puzzlehunt/settings.py
+++ puzzlehunt/puzzlehunt/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -23,6 +24,13 @@ SECRET_KEY = '%0gb7k!_e)6=8%rg5d7m11s6^wq6_d-0wvl7e7tq6gqrq1_ss2'
 DEBUG = True
 
 TEMPLATE_DEBUG = True
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_ROOT, 'templates').replace('\\','/'),
+)
 
 ALLOWED_HOSTS = []
 
@@ -79,4 +87,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
