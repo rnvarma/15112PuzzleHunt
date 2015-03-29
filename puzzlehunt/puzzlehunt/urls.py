2082dfe7c2493ae647d447776e4e9d18848ea2ed
Rohan Varma <rnvarma@andrew.cmu.edu>
2015-03-28 21:19:52 -0400

    Added puzzle to the admin stuff

--- puzzlehunt/puzzlehunt/urls.py
+++ puzzlehunt/puzzlehunt/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from puzzlehunt import views
 
+admin.autodiscover()
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'puzzlehunt.views.home', name='home'),
